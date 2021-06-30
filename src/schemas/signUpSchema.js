a2ed35224502d5088677f7539f7d30c1a8d8f13b
Luiz Fernando <lfaires@gmail.com>
2021-06-29 23:21:48 -0300

    fix: signup schema

--- src/schemas/signUpSchema.js
+++ src/schemas/signUpSchema.js
@@ -6,7 +6,10 @@ const signUpSchema = joi.object({
     .max(30)
     .required(),
     email: joi.string().email().required(),
-    password: joi.string().required(),
+    password: joi.string()
+    .min(6)
+    .max(10)
+    .required(),
     confirmPassword: joi.ref('password')
 });
 
