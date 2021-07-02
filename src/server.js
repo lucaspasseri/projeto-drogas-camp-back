5104909a2675fe234a7ab0f2622aa8b8cdd45d51
Luiz Fernando <lfaires@gmail.com>
2021-07-02 17:49:35 -0300

    prepare deploy

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import app from './app.js';
 
 const port = process.env.PORT;
 
-app.listen(4000, () => {
-    console.log(`Server running on port 4000...`);
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
 });
